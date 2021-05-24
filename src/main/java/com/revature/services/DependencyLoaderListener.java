83d354aec04e7cd3a557d0e0f07bc98da4a972a5
Richard <rtcloud2@gmail.com>
2021-05-23 23:44:41 -0400

    got basic ping/pong app working

---
+++ src/main/java/com/revature/services/DependencyLoaderListener.java
@@ -0,0 +1,22 @@
+package com.revature.services;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class DependencyLoaderListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
