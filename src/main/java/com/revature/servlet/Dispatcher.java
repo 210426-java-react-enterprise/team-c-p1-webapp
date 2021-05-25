bfc6cc394af25bbc37c8fb3de5d0b33d7ac19399
ozzy castillo <oswaldo.castillo@revature.net>
2021-05-24 21:40:15 -0500

    Creating Dispatcher Servlet and UserController class

--- src/main/java/com/revature/servlet/Dispatcher.java
+++ src/main/java/com/revature/servlet/Dispatcher.java
@@ -1,11 +1,30 @@
 package com.revature.servlet;
 
+import controller.UserController;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class Dispatcher extends HttpServlet {
 
 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        switch (req.getRequestURI()){
+            case "/bank/auth.data":
+                UserController.authenticate(req,resp);
+                break;
+            case "/bank/user.data":
 
+                break;
+            default:
+                resp.setStatus(400);
+                resp.getWriter().println(req.getRequestURI());
+        }
+
+    }
 
 
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,10 @@
 package controller;
 
 public class UserController {
+
+    public String authenticate (){
+        
+    }
+
+
 }
