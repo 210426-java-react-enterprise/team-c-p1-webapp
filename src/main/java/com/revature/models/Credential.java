32aef2bb254bc1568ea8d45727ab726be2dfdedb
ozzy castillo <oswaldo.castillo@revature.net>
2021-05-25 00:33:41 -0500

    Adding controller, dispatcher and models

---
+++ src/main/java/com/revature/models/Credential.java
@@ -0,0 +1,67 @@
+package com.revature.models;
+
+import com.revature.orm.MySavable;
+import com.revature.orm.annotations.ColumnType;
+import com.revature.orm.annotations.MyColumn;
+import com.revature.orm.annotations.MyEntity;
+
+@MyEntity(name = "credential")
+public class Credential extends MySavable
+{
+    @MyColumn(  name = "user_name",type = ColumnType.VARCHAR,length = 15,
+            nullable = false,pk = true,fk = false,reference = "",unique = true,delete = "cascade")
+    private String username;
+
+    @MyColumn(  name = "password",type = ColumnType.VARCHAR,length = 50,
+            nullable = false,pk = false,fk = false,reference = "",unique = false,delete = "cascade")
+    private String password;
+
+    @MyColumn(  name = "customer_ssn",type = ColumnType.VARCHAR,length = 12,
+            nullable = false,pk = false,fk = true,reference = "customer.ssn",unique = true,delete = "cascade")
+    private String ssn;
+
+    public Credential(String username, String password, String ssn)
+    {
+        this.username = username;
+        this.password = password;
+        this.ssn = ssn;
+    }
+
+    public Credential(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Credential{");
+        sb.append("username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", ssn='").append(ssn).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
--- src/main/java/com/revature/servlet/Dispatcher.java
+++ src/main/java/com/revature/servlet/Dispatcher.java
@@ -10,18 +10,19 @@ import java.io.IOException;
 
 public class Dispatcher extends HttpServlet {
 
+    private UserController userController = new UserController();
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         switch (req.getRequestURI()){
             case "/bank/auth.data":
-                UserController.authenticate(req,resp);
+                userController.authenticate(req,resp);
                 break;
             case "/bank/user.data":
 
                 break;
             default:
                 resp.setStatus(400);
-                resp.getWriter().println(req.getRequestURI());
+                resp.getWriter().println("We cannot handle this requirement >>> " + req.getRequestURI());
         }
 
     }
