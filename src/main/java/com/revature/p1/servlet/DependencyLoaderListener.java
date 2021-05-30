a35726903098324b9bafba705c946c272929b431
SeanTaba <shahramtaba@gmail.com>
2021-05-30 09:32:54 -0600

    Pipeline test#14

--- src/main/java/com/revature/p1/servlet/DependencyLoaderListener.java
+++ src/main/java/com/revature/p1/servlet/DependencyLoaderListener.java
@@ -16,66 +16,77 @@ import javax.servlet.ServletContextListener;
 
 /**
  * This class is tied to the startup and shutdown of tomcat. Just implement
- *      the ServletContextListener and put whatever logic into the overridden
- *      methods. Make sure you inform tomcat of this class by including it
- *      in your deployment descriptor (web.xml) under the listener tag.
+ * the ServletContextListener and put whatever logic into the overridden
+ * methods. Make sure you inform tomcat of this class by including it
+ * in your deployment descriptor (web.xml) under the listener tag.
  */
-public class DependencyLoaderListener implements ServletContextListener {
+public class DependencyLoaderListener implements ServletContextListener
+{
     @Override
-    public void contextInitialized(ServletContextEvent sce) {
-
+    public void contextInitialized(ServletContextEvent sce)
+    {
         System.out.println("*********************************Starting contextInitialized****************************************");
 
-        Logger logger = LogManager.getLogger();
-        System.out.println("*********************************After logger****************************************");
+        try
+        {
+            System.out.println("*********************************Starting try block****************************************");
 
-        ObjectMapper objectMapper = new JsonMapper();
-        System.out.println("*********************************After objectMapper****************************************");
+            Logger logger = LogManager.getLogger();
+            System.out.println("*********************************After logger****************************************");
 
-        HtmlService htmlService = new HtmlService();
-        System.out.println("*********************************After htmlService****************************************");
+            ObjectMapper objectMapper = new JsonMapper();
+            System.out.println("*********************************After objectMapper****************************************");
 
-        MyObjectRelationalMapper orm = new MyObjectRelationalMapper(ConnectionManager.getInstance()
-                                                                                             .getConnection());
-        System.out.println("*********************************After orm****************************************");
+            HtmlService htmlService = new HtmlService();
+            System.out.println("*********************************After htmlService****************************************");
 
-
-        InputValidator iv = new InputValidator(orm);
-        System.out.println("*********************************After iv****************************************");
-
-        WebUserService webUserService = new WebUserService(orm, iv, logger);
-        System.out.println("*********************************After webUserService****************************************");
+            MyObjectRelationalMapper orm = new MyObjectRelationalMapper(ConnectionManager.getInstance()
+                                                                                         .getConnection());
+            System.out.println("*********************************After orm****************************************");
 
 
+            InputValidator iv = new InputValidator(orm);
+            System.out.println("*********************************After iv****************************************");
+
+            WebUserService webUserService = new WebUserService(orm, iv, logger);
+            System.out.println("*********************************After webUserService****************************************");
 
 
+            WelcomeServlet welcomeServlet = new WelcomeServlet(objectMapper, htmlService);
+            AuthServlet authServlet = new AuthServlet(webUserService, objectMapper, logger, htmlService);
+            AccountsServlet accountsServlet = new AccountsServlet(webUserService, objectMapper, logger, htmlService);
+            NewAccountServlet newAccountServlet = new NewAccountServlet(webUserService, objectMapper, logger, htmlService);
+            AccountServlet accountServlet = new AccountServlet(webUserService, objectMapper, logger, iv, htmlService);
 
-        WelcomeServlet welcomeServlet = new WelcomeServlet(objectMapper,htmlService);
-        AuthServlet     authServlet = new AuthServlet(webUserService, objectMapper, logger,htmlService);
-        AccountsServlet accountsServlet = new AccountsServlet(webUserService,objectMapper, logger,htmlService);
-        NewAccountServlet newAccountServlet = new NewAccountServlet(webUserService, objectMapper, logger,htmlService);
-        AccountServlet accountServlet = new AccountServlet(webUserService, objectMapper, logger, iv,htmlService);
-
-        System.out.println("*********************************After servlet instantiation****************************************");
-
-
-        ServletContext context = sce.getServletContext();
-
-        System.out.println("*********************************After context****************************************");
+            System.out.println("*********************************After servlet instantiation****************************************");
 
 
-        context.addServlet("welcomeServlet", welcomeServlet).addMapping("/");
-        context.addServlet("authServlet", authServlet).addMapping("/login");
-        context.addServlet("accountsServlet", accountsServlet).addMapping("/user/accounts");
-        context.addServlet("newAccountServlet", newAccountServlet).addMapping("/user/accounts/new");
-        context.addServlet("accountServlet", accountServlet).addMapping("/user/account");
+            ServletContext context = sce.getServletContext();
 
-        System.out.println("*********************************End of listener****************************************");
+            System.out.println("*********************************After context****************************************");
 
+
+            context.addServlet("welcomeServlet", welcomeServlet)
+                   .addMapping("/");
+            context.addServlet("authServlet", authServlet)
+                   .addMapping("/login");
+            context.addServlet("accountsServlet", accountsServlet)
+                   .addMapping("/user/accounts");
+            context.addServlet("newAccountServlet", newAccountServlet)
+                   .addMapping("/user/accounts/new");
+            context.addServlet("accountServlet", accountServlet)
+                   .addMapping("/user/account");
+
+            System.out.println("*********************************End of listener****************************************");
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent sce) {
+    public void contextDestroyed(ServletContextEvent sce)
+    {
 
     }
 }
